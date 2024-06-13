4f1cf6a8b799c4590acde913c7939cf612b9d6a5
aniket931 <aniketbhakta61@gmail.com>
2024-06-13 20:02:58 +0530

    connect to database

--- api/index.js
+++ api/index.js
@@ -1,6 +1,17 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
 
 const app = express();
+dotenv.config();
+mongoose.connect(process.env.MONGO)
+    .then(() => {
+        console.log("Connected to Mongo");
+    })
+    .catch((err) => {
+        console.log("Error in mongo connection");
+    });
+
 
 let port = 3000;
 app.listen(port, () => {
