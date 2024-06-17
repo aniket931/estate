90c2c521be554b17e5117674ca23a567912d222c
aniket931 <aniketbhakta61@gmail.com>
2024-06-17 12:01:11 +0530

    render final

--- api/index.js
+++ api/index.js
@@ -5,6 +5,7 @@ import userRouter from "./routes/user.routes.js";
 import authRouter from "./routes/auth.route.js";
 import cookieParser from "cookie-parser";
 import listingRouter from "./routes/listing.route.js";
+import path from 'path';
 
 const app = express();
 app.use(express.json());
@@ -18,6 +19,7 @@ mongoose.connect(process.env.MONGO)
         console.log("err");
     });
 
+const __dirname = path.resolve();
 
 let port = 3000;
 app.listen(port, () => {
@@ -27,6 +29,10 @@ app.listen(port, () => {
 app.use("/api/user", userRouter);
 app.use("/api/auth", authRouter);
 app.use("/api/listing", listingRouter);
+app.use(express.static(path.join(__dirname,'/client/dist')));
+app.get('*', (req,res) => {
+    res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
+})
 app.use((err, req, res, next) => {
     const statusCode = err.statusCode || 500;
     const message = err.message || "Internal server error";
