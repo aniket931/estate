82d095af5b37f41f4eacbc42658debc089028ac9
aniket931 <aniketbhakta61@gmail.com>
2024-06-17 19:17:19 +0530

    fixed listing page

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -164,10 +164,10 @@ export default function Profile() {
             <h1 className='text-center mt-o7 text-2xl font-semibold'>Your Listings</h1>
             {userListings.map((listing) => (
               <div key={listing._id} className="border rounded-lg p-3 flex justify-between items-center gap-4">
-                <Link to={`listing/${listing._id}`}>
+                <Link to={`/listing/${listing._id}`}>
                   <img src={listing.imageUrls[0]} alt="list cover" className='h-16 w-16 object-contain rounded-lg' />
                 </Link>
-                <Link to={`listing/${listing._id}`} className='text-slate-700 font-semibold flex-1 hover:underline truncate'>
+                <Link to={`/listing/${listing._id}`} className='text-slate-700 font-semibold flex-1 hover:underline truncate'>
                   <p>{listing.name}</p>
                 </Link>
                 <div className="flex flex-col items-center">
